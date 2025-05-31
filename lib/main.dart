1649d2dd2ebd3157eeaae006cd12965fbe962898
Mark Achiles <markachilesflores2004@gmail.com>
2025-05-31 16:09:01 +0800

    Modified to my tastes

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
+        appBar: AppBar(title: Text('Dicee'), backgroundColor: Colors.red),
         body: DicePage(),
       ),
     );
@@ -33,6 +30,7 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+  int totalDice = 2;
 
   final random = Random();
 
@@ -40,25 +38,43 @@ class _DicePageState extends State<DicePage> {
     setState(() {
       leftDiceNumber = random.nextInt(6) + 1;
       rightDiceNumber = random.nextInt(6) + 1;
+      totalDice = leftDiceNumber + rightDiceNumber;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 30,
         children: <Widget>[
-          Expanded(
-            child: TextButton(
-              onPressed: randomizeDice,
-              child: Image.asset('images/dice$leftDiceNumber.png')
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+            ],
+          ),
+          Text(
+            'The total is $totalDice',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16
             ),
           ),
-          Expanded(
-            child: TextButton(
-              onPressed: randomizeDice,
-              child: Image.asset('images/dice$rightDiceNumber.png')
-            ),
+          ElevatedButton(
+            onPressed: randomizeDice,
+            child: Text('Roll the dices!'),
           ),
         ],
       ),
